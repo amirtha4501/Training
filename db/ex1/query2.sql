7d194f56d592c4f233d70d569d1d3f7cc8c94ef0
Amirtha <amirtha4501@gmail.com>
2020-08-01 22:39:28 +0530

    Added create, insert, alter and select operation

---
+++ db/ex1/query2.sql
@@ -0,0 +1,32 @@
+-- 2. Write a query to insert at least 5 employees for each department
+
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('1', 'amirtha', 'easwaran', '2001-11-12', '2020-11-21', '10000000', '1');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('2', 'ammu', '2001-11-12', '2020-11-21', '10000', '2');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('3', 'siva', 'kumar', '2001-11-12', '2020-11-21', '4000', '5');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('4', 'kavin', 'kumar', '2001-11-12', '2020-11-21', '45000', '2');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('5', 'lakshmi', '2001-11-12', '2020-11-21', '5000', '1');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('6', 'kannan', '2001-11-12', '2020-11-21', '10000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('7', 'ashok', '2001-11-12', '2020-11-21', '10000', '5');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('8', 'kavitha', '2001-11-12', '2020-11-21', '10000', '1');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('9', 'meera', '2001-11-12', '2020-11-21', '10000', '1');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('10', 'kowsalya', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('11', 'neela', '2001-11-12', '2020-11-21', '10000', '5');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('12', 'kalki', 'nethaji', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('13', 'shivu', '2001-11-12', '2020-11-21', '10000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('14', 'narayan', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('15', 'naveen', 'kumar', '2001-11-12', '2020-11-21', '10000', '5');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('16', 'kishore', 'kumar', '2001-11-12', '2018-11-18', '10000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('17', 'jenifar', '2001-11-12', '2018-11-18', '10000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('18', 'angel ', 'mithra', '2001-11-12', '2018-11-18', '10000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('19', 'nithya', 'shree', '2001-11-12', '2018-11-18', '2000', '4');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('20', 'raj', 'mohan', '2001-11-12', '2018-11-18', '3000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('21', 'nithya', 'menon', '2001-11-12', '2018-11-18', '10000', '5');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('22', 'yoga', 'balajee', '2001-11-12', '2018-11-18', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('23', 'arun', 'kumar', '2001-11-12', '2018-11-18', '10000', '2');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('24', 'gopika', 'ganesan', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('25', 'reethiga', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('26', 'ravi', '2001-11-12', '2020-11-21', '10000', '1');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('27', 'saveetha', '2001-11-12', '2020-11-21', '10000', '2');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('28', 'anita', '2001-11-12', '2020-11-21', '10000', '2');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('29', 'abi', '2001-11-12', '2020-11-21', '10000', '3');
+INSERT INTO `db`.`employee` (`emp_id`, `first_name`, `surname`, `dob`, `date_of_joining`, `annual_salary`, `department_id`) VALUES ('30', 'nandha', 'gopal', '2020-11-21', '2020-11-21', '10000', '3');
---
+++ db/ex1/query3.sql
@@ -0,0 +1,5 @@
+-- 3. Add the Primary key to employee number column
+
+ALTER TABLE `db`.`employee` 
+ADD PRIMARY KEY (`emp_id`);
+;
